ba94084137d3e4e357ad3cbe2ec05b6ee8412c1d
DenisB <bojanovskiida@yandex.ru>
2022-11-04 18:14:39 +0700

    countbits

--- kata/CountBits.go
+++ kata/CountBits.go
@@ -5,3 +5,14 @@ import "math/bits"
 func CountBits(inp uint) int {
 	return bits.OnesCount(inp)
 }
+
+// func CountBits(n uint) int {
+// 	var res int = 0
+// 	for (n>0) {
+// 	  if (n & 1 == 1) {
+// 		res = res + 1
+// 	  }
+// 	  n = n >> 1
+// 	}
+// 	return res
+// }
